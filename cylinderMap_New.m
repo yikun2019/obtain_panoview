bbd9b95a80281cd7161ac02eff6f0fc85819e117
shenzhixintu <laneyikun@foxmail.com>
2017-04-27 16:14:18 +0800

    Update cylinderMap_New.m

--- cylinderMap_New.m
+++ cylinderMap_New.m
@@ -18,30 +18,30 @@ function map=cylinderMap_New(myCylinder, vertex, face)
 
 	[tmp1,~]=size(whole);
     
-    C = zeros(m,n);
+    	C = zeros(m,n);
 
-    %tic
+    	%tic
 	for i=1:tmp1
-        if isempty(whole{i})==0
-		[tmp2,~]=size(whole{i});
-		for j=1:tmp2
-			x=whole{i}(j,1)+1;
-			y=whole{i}(j,2)+1;      
+        	if isempty(whole{i})==0
+			[tmp2,~]=size(whole{i});
+			for j=1:tmp2
+				x=whole{i}(j,1)+1;
+				y=whole{i}(j,2)+1;      
 
-			direction = [X(y,x),Y(y,x),0];     % ray's direction
-        	direction = direction/sqrt(sum((direction.^2)));
-       		[intersect, t, ~,~,~] = TriangleRayIntersection (...
-            	[origX,origY,Z(y,x)], direction, vert1(i,:), vert2(i,:), vert3(i,:));
-        	if sum(intersect) 
-            	[CC,~] = max(t(intersect==1));
-            	if CC>=C(y,x)
-            		C(y,x) = CC;
-                end
-            end
-        end
-        end
-    end
-    %toc
+				direction = [X(y,x),Y(y,x),0];     % ray's direction
+        			direction = direction/sqrt(sum((direction.^2)));
+       				[intersect, t, ~,~,~] = TriangleRayIntersection (...
+            			[origX,origY,Z(y,x)], direction, vert1(i,:), vert2(i,:), vert3(i,:));
+        			if sum(intersect) 
+            				[CC,~] = max(t(intersect==1));
+            				if CC>=C(y,x)
+            					C(y,x) = CC;
+               		 		end
+            			end
+       			end
+       	 	end
+    	end
+    	%toc
 	map = mat2gray(C);
 end
 
